61397c8d470dac5cda0ae06317484c8d8e3355f9
estebanbech <ebdesarrolloweb@gmail.com>
2025-11-03 19:22:53 -0300

    Socio iniciado

--- SportClubApp/Socio.cs
+++ SportClubApp/Socio.cs
@@ -8,5 +8,50 @@ namespace SportClubApp
 {
     internal class Socio : Persona
     {
+        private int nroSocio;
+        private DateTime fechaAlta;
+        private bool habilitado;
+        private bool aptoFisico;
+        public Socio(string nombre, string apellido, string dni, string telefono, string email,
+                     int nroSocio, DateTime fechaAlta, bool habilitado, bool aptoFisico)
+            : base(nombre, apellido, dni, telefono, email)
+        {
+            this.nroSocio = nroSocio;
+            this.fechaAlta = fechaAlta;
+            this.habilitado = habilitado;
+            this.aptoFisico = aptoFisico;
+        }
+        public int GetNumeroSocio()
+        {
+            return nroSocio;
+        }
+        public DateTime GetFechaAlta()
+        {
+            return fechaAlta;
+        }
+        public bool IsHabilitado()
+        {
+            return habilitado;
+        }
+        public bool IsAptoFisico()
+        {
+            return aptoFisico;
+        }
+        public void SetNumeroSocio(int nroSocio)
+        {
+            this.nroSocio = nroSocio;
+        }
+        public void SetFechaAlta(DateTime fechaAlta)
+        {
+            this.fechaAlta = fechaAlta;
+        }
+        public void SetHabilitado(bool habilitado)
+        {
+            this.habilitado = habilitado;
+        }
+        public void SetAptoFisico(bool aptoFisico)
+        {
+            this.aptoFisico = aptoFisico;
+        }
     }
 }
