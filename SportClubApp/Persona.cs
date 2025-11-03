163c4b1f9efec0b04cabd251a5c0da22ff697c85
estebanbech <ebdesarrolloweb@gmail.com>
2025-11-03 19:14:05 -0300

    Clase Persona

---
+++ SportClubApp/Persona.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportClubApp
+{
+    internal class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private string dni;
+        private string telefono;
+        private string email;
+
+        Persona(string nombre, string apellido, string dni, string telefono, string email)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+            this.telefono = telefono;
+            this.email = email;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public string GetApellido()
+        {
+            return apellido;
+        }
+        public string GetDni()
+        {
+            return dni;
+        }
+        public string GetTelefono()
+        {
+            return telefono;
+        }
+        public string GetEmail()
+        {
+            return email;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public void SetApellido(string apellido)
+        {
+            this.apellido = apellido;
+        }
+        public void SetDni(string dni)
+        {
+            this.dni = dni;
+        }
+        public void SetTelefono(string telefono)
+        {
+            this.telefono = telefono;
+        }
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+
+    }
+}
